83d58cb6ef401bd791b09a0a29c79c214ecbea34
Mehmet <mehmethayirli0@gmail.com>
2024-07-04 16:04:00 +0300

    app.js functions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import Login from "./Components/Login";
 import Connected from "./Components/Connected";
+import { contractAbi, contractAddress } from "./Constant/constant";
 
 import "./App.css";
 
@@ -9,8 +10,17 @@ function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(true);
+  const [remainingTime, setremainingTime] = useState("");
+  const [candidates, setCandidates] = useState([]);
 
   useEffect(() => {
+    const init = async () => {
+      getRemainingTime();
+      getCandidates();
+      getCurrentStatus();
+    };
+
     if (window.ethereum) {
       window.ethereum.on("accountsChanged", handleAccountsChanged);
     }
@@ -22,10 +32,11 @@ function App() {
         );
       }
     };
-  });
+  }); //,[account]);
+
   function handleAccountsChanged(accounts) {
-    if (accounts.length > 0 && account !== account[0]) {
-      setAccount(account[0]);
+    if (accounts.length > 0 && account !== accounts[0]) {
+      setAccount(accounts[0]);
     } else {
       setIsConnected(false);
       setAccount(null);
@@ -49,6 +60,46 @@ function App() {
     } else {
       console.error("Tarayıcıda Metamask algılanamadı");
     }
+  }
+
+  async function getCandidates() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    const candidatesList = await contractInstance.getAllVotesOfCandiates();
+    console.log(candidatesList);
+  }
+
+  async function getCurrentStatus() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    const status = await contractInstance.getVotingStatus();
+    console.log(status);
+    setVotingStatus(status);
+  }
+
+  async function getRemainingTime() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    const time = await contractInstance.getRemainingTime();
+    setremainingTime(parseInt(time, 16));
   }
 
   return (
